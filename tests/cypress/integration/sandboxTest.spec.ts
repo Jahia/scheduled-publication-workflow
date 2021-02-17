a90967ff7af6f22098d6d09e7131e4f899ed9851
Francois G <fgerthoffert@jahia.com>
2021-02-16 20:24:52 -0500

    Delete sandboxTest.spec.ts

--- tests/cypress/integration/sandboxTest.spec.ts
+++
@@ -1,8 +0,0 @@
-import { sandboxPage } from '../page-object/sandbox.page'
-
-describe('Successfully navigates to sandbox app', () => {
-    it('successfully goes to token app through dashboard menu and finds create token button', function () {
-        sandboxPage.goTo()
-        expect(sandboxPage.getByText('button', 'Some Action')).not.to.be.undefined
-    })
-})
