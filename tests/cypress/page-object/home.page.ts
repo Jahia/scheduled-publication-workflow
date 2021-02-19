920f7e08c3e237a48062eb19a9fde5caa0fad245
Anton Kovunov <anton.kovunov@gmail.com>
2021-02-19 11:34:21 -0500

    BACKLOG-15596 added reviewer test

--- tests/cypress/page-object/home.page.ts
+++ tests/cypress/page-object/home.page.ts
@@ -1,10 +1,28 @@
 import { BasePage } from './base.page'
+import * as dayjs from 'dayjs';
 
 class HomePage extends BasePage {
+    public static readonly EDITOR_NAME_AND_PASSWORD = 'editor'
+
     goTo(user?: authMethod) {
         cy.goTo('/sites/digitall/home.html', user)
         cy.goTo('/cms/edit/default/en/sites/digitall/home.html?redirect=false', user)
         return cy
+    }
+
+    publishContentAsEditorAndValidate() {
+        this.login(HomePage.EDITOR_NAME_AND_PASSWORD, HomePage.EDITOR_NAME_AND_PASSWORD, BasePage.SITE);
+        home.goTo({username: HomePage.EDITOR_NAME_AND_PASSWORD, password: HomePage.EDITOR_NAME_AND_PASSWORD});
+        home.getIframeBody().contains('global network', {matchCase: false}).should(this.BE_VISIBLE);
+        home.getIframeBody().get('.toolbar-item-publishone.action-bar-tool-item').click();
+        const workflowactiondialog = home.getIframeBody().get('.workflowactiondialog-card');
+        workflowactiondialog.get('input[name="date"]').type(dayjs().add(5, 'minute').format('DD.MM.YYYY HH:mm'));
+        workflowactiondialog
+            .get('.x-panel-bbar')
+            .contains('Request publication', {matchCase: false})
+            .should('be.visible')
+            .click();
+        home.goTo({username: HomePage.EDITOR_NAME_AND_PASSWORD, password: HomePage.EDITOR_NAME_AND_PASSWORD});
     }
 }
 
---
+++ tests/cypress/page-object/workflow.page.ts
@@ -0,0 +1,28 @@
+import { BasePage } from './base.page'
+
+class WorkflowPage extends BasePage {
+    public static readonly SITE = 'digitall';
+    public static readonly REVIEWER_USERNAME = 'ace';
+    public static readonly REVIEWER_PASSWORD = 'ventura';
+
+    goTo(user?: authMethod) {
+        cy.goTo('/jahia/workflow', user)
+        return cy
+    }
+
+    openWorkflowAndVerifyButtons() {
+        this.login(WorkflowPage.REVIEWER_USERNAME, WorkflowPage.REVIEWER_PASSWORD, WorkflowPage.SITE);
+        this.goTo({username: WorkflowPage.REVIEWER_PASSWORD, password: WorkflowPage.REVIEWER_PASSWORD})
+        this.getByText('label', 'en - Default started by editor').should(this.BE_VISIBLE)
+        cy.get('.x-tree3-node-joint').click()
+        this.getByText('button', 'Review').should(this.BE_VISIBLE).click()
+        this.getByText('button', 'Reject').should(this.BE_VISIBLE)
+        this.getByText('button', 'Accept').should(this.BE_VISIBLE)
+        this.getByText('button', 'Publish now').should(this.BE_VISIBLE)
+        this.getByText('button', 'Cancel').should(this.BE_VISIBLE)
+        cy.get('input[name=date]').should(this.BE_VISIBLE)
+        cy.get('input[name=date]').should('have.attr', 'readonly')
+    }
+}
+
+export const workflowPage = new WorkflowPage()
