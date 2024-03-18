389adcd63b24ca05d4a42ef7e6c523eccb8e535e
Francois G <fgerthoffert@jahia.com>
2024-03-18 09:54:27 +0100

    TECH-1657: Removed disabled tests

--- tests/assets/setMailServer.groovy
+++
@@ -1,9 +0,0 @@
-import org.jahia.registries.ServicesRegistry
-import org.jahia.services.mail.MailSettings
-
-ServicesRegistry.instance.mailService.store(new MailSettings(
-        true,
-        "smtp://mailhog.test:1025?mail.debug=true",
-        "jahia.server@test.com",
-        "jahia.notifications@test.com",
-        "Disabled"));
